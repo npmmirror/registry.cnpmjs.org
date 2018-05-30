f38850031f617e8d88df6649f907cb9806955beb
Suyi <thonatos.yang@gmail.com>
2018-05-30 23:23:52 +0800

    test: use async

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -4,12 +4,12 @@ const { app, assert } = require('egg-mock/bootstrap');
 
 describe('test/app/controller/home.test.js', () => {
 
-  it('should assert', function* () {
+  it('should assert', async () => {
     const pkg = require('../../../package.json');
     assert(app.config.keys.startsWith(pkg.name));
 
     // const ctx = app.mockContext({});
-    // yield ctx.service.xx();
+    // await ctx.service.xx();
   });
 
   it('should GET /', () => {
